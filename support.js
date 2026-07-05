274ebe379ae62bbad79374e99d10bbd19a51b4dc
yourShika <88596098+yourShika@users.noreply.github.com>
2026-07-05 15:19:00 +0200

    Add files via upload

---
+++ support.js
@@ -0,0 +1,10 @@
+{
+  "updated": "2026-07-05T00:00:00Z",
+  "source": "https://na.finalfantasyxiv.com/lodestone/freecompany/9279948507173621416/member/",
+  "members": [
+    { "id": "51140687", "name": "Akeno Kusuri-ya", "rank": "Kaiser Shiba", "level": 100, "avatar": "" },
+    { "id": "11359811", "name": "Maomao Choi", "rank": "Princess", "level": 100, "avatar": "" },
+    { "id": "45171724", "name": "Yavi Wohtu", "rank": "Princess", "level": 27, "avatar": "" },
+    { "id": "58116762", "name": "Akeno Saeki", "rank": "NekoPaw", "level": 25, "avatar": "" }
+  ]
+}
